e067991b0d13e3380f9142bbd4948d48e2d4b19a
Attila Vago <attilavago85@gmail.com>
2017-02-11 15:51:51 +0000

    working prototype

--- src/app.js
+++ src/app.js
@@ -1,3 +1,45 @@
+const fs = require('fs');
+
+
+var folder_path = '';
+var leftSoundsArray = [];
+var rightSoundsArray = [];
+
+function chooseDestFolder(){
+  var chooser = $('#folder_dialog_input');
+    chooser.unbind('change'); // Needed, otherwise the value will always be "" 
+    chooser.change(function(evt) {
+        folder_path = $(this).val();
+        $(this).val(''); // Reset value of selected directory (so change event will *always* be triggered)
+        console.log(folder_path);
+    });
+    chooser.trigger('click'); 
+}
+
+$('#mergeFiles').click(function(){
+  for(var i = 0; i < leftSoundsArray.length; i++){
+    var stream;
+    //var soundPair = [];
+    //soundPair = soundPair.push(leftSoundsArray[i]);
+    //soundPair = soundPair.push(rightSoundsArray[i]);
+    var newFile = fs.createWriteStream(`${folder_path}/newFile${i}.mp3`);
+    stream = fs.createReadStream(leftSoundsArray[i]);
+    stream.pipe(newFile, {end: false});
+    stream = fs.createReadStream(rightSoundsArray[i]);
+    stream.pipe(newFile, {end: false});
+    stream.on('end', function() {
+        console.log('added file', stream);  
+    });
+    //console.log('new file:', leftSoundsArray[i]+rightSoundsArray[i]);
+    //
+    //stream.pipe(dhh, {end: false});
+  }
+});
+
+$('#chooseDest').click(function(){
+  chooseDestFolder('#folder_dialog_input');
+});
+
 $('#left-trigger').click(function(){
   chooseFileLeft('#fileDialogLeft');
 });
@@ -13,7 +55,9 @@ $('#right-trigger').click(function(){
       var files = $('#fileDialogLeft')[0].files;
 		for (var i = 0; i < files.length; ++i){
 			console.log(files[i].name);
-  			$('#left-file-pool').append(`<a href="${files[i].path}">${files[i].name}</a>`);
+  			$('#left-file-pool').append(`<p data-source="${files[i].path}">${files[i].name}</p>`);
+        leftSoundsArray.push(files[i].path);
+        console.log(leftSoundsArray);
 		}
     });
 
@@ -27,10 +71,59 @@ $('#right-trigger').click(function(){
       var files = $('#fileDialogRight')[0].files;
     for (var i = 0; i < files.length; ++i){
       console.log(files[i].name);
-        $('#right-file-pool').append(`<a href="${files[i].path}">${files[i].name}</a>`);
+        $('#right-file-pool').append(`<p data-source="${files[i].path}">${files[i].name}</p>`);
+        rightSoundsArray.push(files[i].path);
+        console.log(rightSoundsArray);
     }
     });
 
     chooser.trigger('click');  
   }
+
+
+
+// audio concat feature 
+/*
+
+var fs = require('fs'),
+    files = fs.readdirSync('./files'),
+    clips = [],
+    stream,
+    currentfile,
+    dhh = fs.createWriteStream('./dhh-interview.mp3');
+
+// create an array with filenames (time)
+files.forEach(function (file) {
+    clips.push(file.substring(0, 6));  
+});
+
+// Sort
+clips.sort(function (a, b) {
+    return a - b;
+});
+
+
+// recursive function
+function main() {
+    if (!clips.length) {
+        dhh.end("Done");
+        return;
+    }
+    
+    currentfile = './files/' + clips.shift() + '.mp3';
+    stream = fs.createReadStream(currentfile);
+    
+    stream.pipe(dhh, {end: false});
+    
+    stream.on("end", function() {
+        console.log(currentfile + ' appended');
+        main();        
+    });
+}
+
+
+main();
+*/
+
+
  
